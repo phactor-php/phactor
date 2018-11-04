357689c6f858a2112cbc3d0365b187895788eb64
Carnage <t.carnage@gmail.com>
2018-11-04 10:53:15 +0000

    Added message provders to bus factory

--- src/Zend/BusFactory.php
+++ src/Zend/BusFactory.php
@@ -11,12 +11,19 @@ use Interop\Container\ContainerInterface;
 use Zend\Log\Logger;
 use Zend\Log\Writer\Noop;
 use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\Stdlib\ArrayUtils;
 
 class BusFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $subscriptions = $container->get('Config')['message_subscriptions'];
+        $providers = $container->get('Config')['message_subscription_providers'];
+
+        foreach ($providers as $provider) {
+            $subscriptions = ArrayUtils::merge($subscriptions, $provider::getSubscriptions());
+        }
+
         $genericBus = new GenericBus(
         //$container->get('Log'),
             (new Logger())->addWriter(new Noop()),
